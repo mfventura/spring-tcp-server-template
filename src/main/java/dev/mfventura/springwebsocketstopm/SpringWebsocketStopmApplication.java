129ebb057e07412a998458fdbdae63ee4307bb27
mfventura <manuel.fdz.ventura@gmail.com>
2023-12-06 11:00:14 +0000

    init project

---
+++ src/main/java/dev/mfventura/springwebsocketstopm/SpringWebsocketStopmApplication.java
@@ -0,0 +1,12 @@
+package dev.mfventura.springwebsocketstopm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketStopmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketStopmApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/mfventura/springwebsocketstopm/SpringWebsocketStopmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mfventura.springwebsocketstopm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketStopmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
